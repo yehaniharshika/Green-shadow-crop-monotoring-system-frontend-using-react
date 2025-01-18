8485fda8a1e78255c9d4da8ef7a3aef6905a5792
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-01-18 09:37:38 +0530

    create Field.tsx with adding basic styles

--- src/pages/Field.tsx
+++ src/pages/Field.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, Modal, Form } from 'react-bootstrap';
-import { MdAddLocationAlt } from 'react-icons/md';
+import {Button, Modal, Form, Col, Container, Row, Table} from 'react-bootstrap';
 import { FaCamera } from 'react-icons/fa';
-import { Image } from 'react-bootstrap';
 import { Navigation } from "../components/Navigation";
+import "../components/Field.css";
+
 
 const Field = () => {
     const [fieldCode, setFieldCode] = useState('');
@@ -50,105 +50,133 @@ const Field = () => {
         handleClose(); // Close the modal
     };
 
-    // Handle Image Selection
-    const handleImageChange1 = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            setFieldImage1(URL.createObjectURL(file)); // Set preview URL
-        }
-    };
 
-    const handleImageChange2 = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            setFieldImage2(URL.createObjectURL(file)); // Set preview URL
-        }
-    };
 
     return (
-        <div className="flex">
-            <Navigation />
-            <Button variant="primary" onClick={handleShow} className="h-1/5">
-                <MdAddLocationAlt /> Add Field
-            </Button>
+        <div className="flex bg-emerald-200">
+            <Navigation/>
+            <div className="flex-1 p-5">
+                <Container fluid>
+                    <Row className="align-items-center mb-3">
+                        <Col md={12}>
+                            <div className="bg-red-800 p-3 rounded top-50">
+                                <Container fluid>
+                                    <Row className="align-items-center">
+                                        <h1 className="font-bold text-2xl text-neutral-100" style={{fontFamily: "'Ubuntu', sans-serif"}}>Field Management</h1>
+                                    </Row>
+                                </Container>
+                            </div>
+                        </Col>
+                    </Row>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Field Details Form</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="container mx-auto bg-red-400 p-4">
-                        <Form>
-                            <Form.Group className="mb-3" controlId="field-code">
-                                <Form.Label className="font-bold" style={{ fontFamily: "'Ubuntu', sans-serif" }}>Field Code</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Field Code" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="field-name">
-                                <Form.Label className="font-bold" style={{ fontFamily: "'Ubuntu', sans-serif" }}>Field Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Field Name" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="field-location">
-                                <Form.Label className="font-bold" style={{ fontFamily: "'Ubuntu', sans-serif" }}>Field Location</Form.Label>
-                                <Form.Control as="textarea" rows={2} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="field-size">
-                                <Form.Label className="font-bold" style={{ fontFamily: "'Ubuntu', sans-serif" }}>Field Size (Sq. m)</Form.Label>
-                                <Form.Control type="number" placeholder="Enter Field Size (Sq. m)" />
-                            </Form.Group>
+                    <br/><br/>
+                    <Button variant="primary" onClick={handleShow} className="h-1/5 w-1/12 font-bold " style={{fontFamily: "'Ubuntu', sans-serif"}}>
+                        + Add Field
+                    </Button>
 
-                            {/* Image Upload Section 1 */}
-                            <Form.Group className="mb-3">
-                                <Form.Label htmlFor="fieldImage1">Field Image 1</Form.Label>
-                                <div className="image-container">
-                                    <div className="image-box">
-                                        {fieldImage1 &&
-                                            <Image id="previewImage1" src={fieldImage1} alt="Field Image 1 Preview" fluid />
-                                        }
-                                    </div>
-                                    <label htmlFor="fieldImage1" className="choose-image-btn">
-                                        <FaCamera size={25} /> Choose Image
-                                    </label>
-                                    <Form.Control
-                                        type="file"
-                                        className="d-none"
-                                        id="fieldImage1"
-                                        accept="image/*"
-                                        onChange={handleImageChange1}
-                                    />
-                                </div>
-                            </Form.Group>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Field Details Form</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <div className="container mx-auto bg-red-400 p-4">
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="field-code">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}>Field
+                                            Code</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter Field Code"/>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="field-name">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}>Field
+                                            Name</Form.Label>
+                                        <Form.Control type="text" placeholder="Enter Field Name"/>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="field-location">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}>Field
+                                            Location</Form.Label>
+                                        <Form.Control as="textarea" rows={2}/>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="field-size">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}>Field
+                                            Size (Sq. m)</Form.Label>
+                                        <Form.Control type="number" placeholder="Enter Field Size (Sq. m)"/>
+                                    </Form.Group>
 
-                            {/* Image Upload Section 2 */}
-                            <Form.Group className="mb-3">
-                                <Form.Label htmlFor="fieldImage2">Field Image 2</Form.Label>
-                                <div className="image-container">
-                                    <div className="image-box">
-                                        {fieldImage2 &&
-                                            <Image id="previewImage2" src={fieldImage2} alt="Field Image 2 Preview" fluid />
-                                        }
-                                    </div>
-                                    <label htmlFor="fieldImage2" className="choose-image-btn">
-                                        <FaCamera size={25} /> Choose Image
-                                    </label>
-                                    <Form.Control
-                                        type="file"
-                                        className="d-none"
-                                        id="fieldImage2"
-                                        accept="image/*"
-                                        onChange={handleImageChange2}
-                                    />
-                                </div>
-                            </Form.Group>
-                        </Form>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button className="bg-blue-500 text-white p-2 rounded" onClick={handleAddField}>Save</Button>
-                    <Button variant="secondary" onClick={handleClose}>Update</Button>
-                    <Button variant="secondary" onClick={handleClose}>Delete</Button>
-                    <Button variant="secondary" onClick={handleClose}>Close</Button>
-                </Modal.Footer>
-            </Modal>
+                                    {/* Image Upload Section 1 */}
+                                    <Form.Group className="mb-3">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}
+                                                    htmlFor="fieldImage1">Field Image 01</Form.Label>
+                                        <div className="image-container">
+                                            <div className="image-box">
+                                                {fieldImage1 && <img src={fieldImage1} alt="Field Image 1 Preview"/>}
+                                            </div>
+                                            <label htmlFor="fieldImage1" className="choose-image-btn">
+                                                <FaCamera size={25}/> Choose Image
+                                            </label>
+                                            <input type="file" className="d-none" id="fieldImage1" accept="image/*"/>
+                                        </div>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label className="font-bold" style={{fontFamily: "'Ubuntu', sans-serif"}}
+                                                    htmlFor="fieldImage1">Field Image 02</Form.Label>
+                                        <div className="image-container">
+                                            <div className="image-box">
+                                                {fieldImage1 && <img src={fieldImage1} alt="Field Image 1 Preview"/>}
+                                            </div>
+                                            <label htmlFor="fieldImage1" className="choose-image-btn">
+                                                <FaCamera size={25}/> Choose Image
+                                            </label>
+                                            <input type="file" className="d-none" id="fieldImage1" accept="image/*"/>
+                                        </div>
+                                    </Form.Group>
+
+                                </Form>
+                            </div>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button className="bg-blue-500 text-white p-2 rounded" onClick={handleAddField}>Save</Button>
+                            <Button variant="secondary" onClick={handleClose}>Update</Button>
+                            <Button variant="secondary" onClick={handleClose}>Delete</Button>
+                            <Button variant="secondary" onClick={handleClose}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
+                    {/* Chart Section */}
+
+                    <br/><br/>
+                    <section
+                        className="field-table-view col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 col-xxl-10"
+                        style={{overflowX: "auto", overflowY: "auto", maxHeight: "540px", width: "96%"}}
+                    >
+                        <div className="table-responsive">
+                            <Table striped bordered responsive>
+                                <thead>
+                                <tr>
+                                    <th scope="col">Field Code</th>
+                                    <th scope="col">Field Name</th>
+                                    <th scope="col">Field Location</th>
+                                    <th scope="col">Field Size</th>
+                                    <th scope="col">Field Image 01</th>
+                                    <th scope="col">Field Image 02</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {/* Example Row (replace with dynamic rows later) */}
+                                <tr>
+                                    <td>ST001</td>
+                                    <td>John</td>
+                                    <td>Doe</td>
+                                    <td>Male</td>
+                                    <td>12</td>
+                                    <td>Main Lane</td>
+                                </tr>
+                                </tbody>
+                            </Table>
+                        </div>
+                    </section>
+
+                </Container>
+            </div>
+
         </div>
     );
 };
