e126922b4ef52c59e63376bcde94fdd5201ca308
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-01-19 09:47:27 +0530

    create CropSlice.ts

---
+++ src/reducers/CropSlice.ts
@@ -0,0 +1,37 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {Crop} from "../models/Crop.ts";
+
+export interface CropState {
+    crops: Crop[];
+}
+
+const initialState: CropState = {
+    crops: [],
+}
+
+const cropSlice = createSlice({
+    name: 'crop',
+    initialState,
+    reducers: {
+
+        addCrop: (state: CropState, action: PayloadAction<Crop>) => {
+            state.crops.push(action.payload);
+        },
+
+        updateCrop(state, action: PayloadAction<Crop>) {
+            const index = state.crops.findIndex(
+                (crop) => crop.cropCode === action.payload.cropCode
+            );
+            if (index !== -1) {
+                state.crops[index] = action.payload;
+            }
+        },
+
+        deleteCrop(state, action: PayloadAction<string>) {
+            state.crops = state.crops.filter((crop) => crop.cropCode !== action.payload);
+        },
+    },
+});
+export const {addCrop,updateCrop,deleteCrop} = cropSlice.actions;
+
+export default cropSlice.reducer;
